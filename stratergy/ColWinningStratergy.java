f9329de7bb1088fdaaa5a11c025eceb9696278cf
Kunjan <kunjan.gondalia.1@gmail.com>
2024-12-31 16:40:25 +0530

    Col Winning Strategy Implemented

--- stratergy/ColWinningStratergy.java
+++ stratergy/ColWinningStratergy.java
@@ -1,10 +1,26 @@
 package TicTacToe.stratergy;
 import TicTacToe.models.*;
+import java.util.*;
 public class ColWinningStratergy implements WinningStratergy{
 
+    HashMap<Integer, HashMap<Character, Integer>> colCount = new HashMap<>();
     @Override
     public boolean checkWinner(Board board, Move move) {
-        System.out.println("Checking the winner in row");
+
+        int col = move.getCell().getCol();
+        Character symbol = move.getPlayer().getSymbol().getSymChar();
+
+        if(!colCount.containsKey(col)){
+            colCount.put(col,new HashMap<>());
+        }
+
+        HashMap<Character,Integer> count = colCount.get(col);
+        count.put(symbol,count.getOrDefault(symbol,0)+1);
+
+        if(count.get(symbol)==board.getGrid().size()){
+            return true;
+        }
+
         return false;
     }
 }
